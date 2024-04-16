bd38b0b8de773487c248be091586e58d3b0b06fb
Kedar Gavali <152150880+Kedar37@users.noreply.github.com>
2024-04-16 11:42:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let container = document.querySelector('.inner-container');
+container.addEventListener('mouseover', (event) => {
+    container.style.clipPath = 'circle(100% at center center)'
+});
